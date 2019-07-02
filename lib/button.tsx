5338b2af2950273256d94a4696bfaacf009fb261
陈晓拉尼 <2524729719@qq.com>
2019-07-02 19:51:21 +0800

    更改package里面的快速启动命令， 并且安装react的类型声明文件

---
+++ lib/button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class button extends React.Component {
+    render() {
+        return (
+            <div>
+                按钮
+            </div>
+        )
+    }
+}

7a895f971c7ec4258552020bfb2d9e342083ceb2
陈晓拉尼 <2524729719@qq.com>
2019-07-04 01:06:15 +0800

    区分开发环境和生产环境

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
-    mode: 'production',
+    // mode: 'production',
     entry: {
         index: './lib/index.tsx'
     },
@@ -20,23 +20,23 @@ module.exports = {
             loader: 'awesome-typescript-loader'
         }]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        })
-    ],
-    externals: {
-        react: {
-            commonjs: "react",
-            commonjs2: "react",
-            amd: "react",
-            root: "React"
-        },
-        'react-dom': {
-            commonjs: 'react-dom',
-            commonjs2: 'react-dom',
-            amd: 'react-dom',
-            root: "ReactDOM"
-        }
-    }
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         template: 'index.html'
+    //     })
+    // ],
+    // externals: {
+    //     react: {
+    //         commonjs: "react",
+    //         commonjs2: "react",
+    //         amd: "react",
+    //         root: "React"
+    //     },
+    //     'react-dom': {
+    //         commonjs: 'react-dom',
+    //         commonjs2: 'react-dom',
+    //         amd: 'react-dom',
+    //         root: "ReactDOM"
+    //     }
+    // }
 }

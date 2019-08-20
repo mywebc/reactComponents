3f1f3110fdfb0b42e67cfdec52847c9504fcdc16
陈晓拉尼 <2524729719@qq.com>
2019-08-20 16:18:37 +0800

    支持传className，内部已处理，可以合并

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './importIcons.js'
 import './icon.scss'
-
+import classes from './helpers/classes'
 
 interface IconProps extends React.SVGAttributes<SVGAElement>{
     name: string,
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
+    const {className, ...rest} = props
     return (
-        <svg className="gui-icon" {...props}>
+        <svg className={classes("gui-icon", className)} {...rest}>
             <use xlinkHref={`#${props.name}`}></use>
         </svg>
     )
